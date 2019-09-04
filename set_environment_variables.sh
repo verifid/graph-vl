46ad45d6facb316c3767eafaa6d1ed286e85ee7b
Abdullah Selek <abdullahselek@gmail.com>
2019-09-04 19:53:38 +0100

    Add file as parameter on bash script.

--- set_environment_variables.sh
+++ set_environment_variables.sh
@@ -1,4 +1,20 @@
-cat env-postgres.env | while read line; do
+#!/bin/bash
+
+FILE=$1
+
+if [ -z "$FILE" ]
+then
+    echo "Give file name as parameter!"
+    return 0
+fi
+
+if [ ! -f "$FILE" ]
+then
+    echo "$FILE not exists"
+    return 0
+fi
+
+cat "$FILE" | while read line; do
     echo $line
     export $line
 done
