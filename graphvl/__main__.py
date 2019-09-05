94808ff2dcc0527cbadd035ddcca5955e7b7273f
Abdullah Selek <abdullahselek@gmail.com>
2019-09-05 20:08:16 +0100

    Add new option on main module.

--- graphvl/__main__.py
+++ graphvl/__main__.py
@@ -12,6 +12,7 @@ from graphvl.db import init_db
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='CLI - Self deployed identity verification layer with GraphQL.')
+    parser.add_argument('-v', '--variables', type=str, help='Sets environment variables')
     parser.add_argument('-t', '--table', help='Creates database tables', action='store_true')
     args = parser.parse_args()
 
@@ -23,6 +24,9 @@ if __name__ == '__main__':
         pass
 
     args = parser.parse_args()
+    if args.variables:
+        command = 'source ./set_environment_variables.sh ' + args.variables
+        os.system(command)
     if args.table:
-        os.system('source ./set_environment_variables.sh env-postgres.env')
         init_db.init_db()
+        print('Database tables created!')
