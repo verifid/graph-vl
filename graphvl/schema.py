9fdddc80c9ee95ba61174be286dab7f815f612c7
Abdullah Selek <abdullahselek@gmail.com>
2019-09-07 21:27:41 +0100

    Save user on database.

--- graphvl/schema.py
+++ graphvl/schema.py
@@ -6,15 +6,23 @@ import graphene
 
 from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
+
 from graphvl.scalar import Date
 from graphvl.utils import utils
+from graphvl.models.user import UserCreate
+from graphvl import crud
+from graphvl.db.session import db_session
+
+from datetime import datetime
+
 
 class User(graphene.ObjectType):
     country = graphene.String(required=True, description='Living country of user')
     date_of_birth = Date(required=True, description='Born date of user in format of dd-MM-yyyy')
     name = graphene.String(required=True, description='Name of user')
     surname = graphene.String(required=True, description='Surname of user')
-    unique_id = graphene.String(description='Unique id defined by api')
+    user_id = graphene.String(description='Unique id defined by api')
+
 
 class CreateUser(graphene.Mutation):
     class Arguments:
@@ -28,16 +36,20 @@ class CreateUser(graphene.Mutation):
 
     def mutate(root, info, country, date_of_birth, name, surname):
         user_id = utils.create_user_id()
-        user = User(country=country,
-                    date_of_birth=date_of_birth,
-                    name=name,
-                    surname=surname,
-                    unique_id=user_id)
+        date = date_of_birth.strftime('%d/%m/%Y')
+        user_in = UserCreate(user_id=user_id,
+                             name=name,
+                             surname=surname,
+                             date_of_birth=date,
+                             country=country)
+        user = crud.user.create(db_session, user_in=user_in)
         ok = True
         return CreateUser(user=user, ok=ok)
 
+
 class UserMutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+
 
 class Query(graphene.ObjectType):
     user = graphene.Field(User, description='User object')
