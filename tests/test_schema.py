f2ce17ba8e1e2f07c3bfe2d3f49691d59b43e6f7
Abdullah Selek <abdullahselek@gmail.com>
2019-09-09 23:07:38 +0100

    Add unit test for create image schema.

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -7,6 +7,8 @@ import graphene
 from collections import OrderedDict
 from graphene.test import Client
 from graphvl.schema import UserQuery, UserMutation
+from graphvl.schema import ImageQuery, ImageMutation
+
 
 class SchemaTest(unittest.TestCase):
 
@@ -24,9 +26,9 @@ class SchemaTest(unittest.TestCase):
                                             }
                                         }
                                     }''')
-        assert executed == {'data': 
+        assert executed == {'data':
                                 OrderedDict(
-                                    [('createUser', 
+                                    [('createUser',
                                         OrderedDict([('ok', True),
                                         ('user',
                                             OrderedDict([('country', 'country'),
@@ -34,6 +36,28 @@ class SchemaTest(unittest.TestCase):
                                             ('name', 'name'),
                                             ('surname', 'surname')])
                                         )])
+                                    )]
+                                )
+                            }
+
+
+    def test_create_image(self):
+        schema = graphene.Schema(query=ImageQuery, mutation=ImageMutation)
+        client = Client(schema)
+        executed = client.execute('''mutation {
+                                        createImage(imageStr: "txt", imageType: 1, userId: "1145b78f-5b7a-43e0-9437-2033bd880769") {
+                                            ok
+                                            image {
+                                            userId
+                                            }
+                                        }
+                                    }''')
+        assert executed == {'data':
+                                OrderedDict(
+                                    [('createImage',
+                                        OrderedDict([('ok', True),
+                                        ('image', OrderedDict([('userId', '1145b78f-5b7a-43e0-9437-2033bd880769')]))]
+                                        )
                                     )]
                                 )
                             }
