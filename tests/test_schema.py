a48f8b609d47da3a957b812aa052b92dfacf7f53
Abdullah Selek <abdullahselek@gmail.com>
2019-09-12 15:20:17 +0100

    Change formatting and read userId.

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -13,7 +13,7 @@ from graphvl.schema import ImageQuery, ImageMutation
 
 class SchemaTest(unittest.TestCase):
 
-    @pytest.mark.run(order=1)
+
     def test_create_user(self):
         schema = graphene.Schema(query=UserQuery, mutation=UserMutation)
         client = Client(schema)
@@ -41,26 +41,40 @@ class SchemaTest(unittest.TestCase):
                                     )]
                                 )
                             }
-        self.user_id = executed['data']['createUser']['user']['userId']
 
 
-    @pytest.mark.run(order=2)
     def test_create_image(self):
+        user_schema = graphene.Schema(query=UserQuery, mutation=UserMutation)
+        user_client = Client(user_schema)
+        executed = user_client.execute('''mutation {
+                                            createUser(country: "country", dateOfBirth: "01-01-1960", name: "name", surname: "surname") {
+                                                ok
+                                                user {
+                                                    country
+                                                    dateOfBirth
+                                                    name
+                                                    surname
+                                                    userId
+                                                }
+                                            }
+                                        }''')
+        self.assertIsNotNone(executed['data']['createUser']['user'])
+        user_id = executed['data']['createUser']['user']['userId']
         schema = graphene.Schema(query=ImageQuery, mutation=ImageMutation)
         client = Client(schema)
         executed = client.execute('''mutation {
-                                        createImage(imageStr: "txt", imageType: 1, userId: "{}") {
+                                        createImage(imageStr: "txt", imageType: 1, userId: "%s") {
                                             ok
                                             image {
                                             userId
                                             }
                                         }
-                                    }'''.format(self.user_id))
+                                    }''' % user_id)
         assert executed == {'data':
                                 OrderedDict(
                                     [('createImage',
                                         OrderedDict([('ok', True),
-                                        ('image', OrderedDict([('userId', '1145b78f-5b7a-43e0-9437-2033bd880769')]))]
+                                        ('image', OrderedDict([('userId', '%s' % user_id)]))]
                                         )
                                     )]
                                 )
