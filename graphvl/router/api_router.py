1d8065d5749a73fc54a4841e6887ee4a815db515
Abdullah Selek <abdullahselek@gmail.com>
2019-09-15 22:44:12 +0100

    Add root endpoint that lists all endpoints.

--- graphvl/router/api_router.py
+++ graphvl/router/api_router.py
@@ -5,11 +5,40 @@ import graphene
 
 from fastapi import APIRouter
 from starlette.graphql import GraphQLApp
+from starlette.responses import Response
 
 from graphvl.schema import UserQuery, ImageQuery
 from graphvl.schema import UserMutation, ImageMutation
 
 
-router = APIRouter() 
+router = APIRouter()
+
+
+@router.get("/")
+async def read_links():
+    response = Response('''<html>
+                            <body>
+                                <head>
+                                    <style>
+                                        h2 {
+                                            color: black;
+                                            font-family: arial;
+                                        }
+                                        a {
+                                            color: black;
+                                            font-family: arial;
+                                        }
+                                    </style>
+                                </head>
+                                <h2>VerifID - Identity Verification Layer</h2>
+                                <ul>
+                                    <li><a href="/user">User endpoint</a></li>
+                                    <li><a href="/image">Image endpoint</a></li>
+                                </ul>
+                             </body>
+                            </html>''', media_type='text/html')
+    return response
+
+
 router.add_route('/user', GraphQLApp(schema=graphene.Schema(query=UserQuery, mutation=UserMutation)))
 router.add_route('/image', GraphQLApp(schema=graphene.Schema(query=ImageQuery, mutation=ImageMutation)))
